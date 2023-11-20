71964d2544ad55962edbd0b58cbf3c7abde78ebf
Sourabh Bindal <95605311+bindalsourabh1@users.noreply.github.com>
2023-11-20 20:41:57 +0530

    Add Bootstrap and React-Bootstrap dependencies, update page title, and fix typo in Sidebar component

--- chess-base/src/components/Sidebar.jsx
+++ chess-base/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar({openSidebarToggle, OpenSidebar}) {
     <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
         <div className='sidebar-title'>
             <div className='sidebar-brand'>
-                <BsCart3  className='icon_header'/> SHOP
+                <BsCart3  className='icon_header'/> 
             </div>
             <span className='icon close_icon' onClick={OpenSidebar}>X</span>
         </div>
@@ -36,7 +36,7 @@ function Sidebar({openSidebarToggle, OpenSidebar}) {
             </li>
             <li className='sidebar-list-item'>
                 <a href="">
-                    <BsListCheck className='icon'/> Mathces
+                    <BsListCheck className='icon'/> Matches
                 </a>
             </li>
             <li className='sidebar-list-item'>
